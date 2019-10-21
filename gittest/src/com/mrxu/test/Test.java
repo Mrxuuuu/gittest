b9506cb9d5d73b63ba82f34b6af7409492341c93
qing_321 <qing_321@163.com>
2019-10-21 17:42:28 +0800

    [update]xu

---
+++ gittest/src/com/mrxu/test/Test.java
@@ -0,0 +1,9 @@
+package com.mrxu.test;
+
+public class Test {
+    public static void main(String[] args) {
+        String name="BOB";
+        int age =14;
+        System.out.println(name+age);
+    }
+}
